9ac83e6ebb567c5e008d5f968405c225475a983c
Idhibhat Pankam <marginpankam@gmail.com>
2024-06-25 21:36:16 +0700

    feat: stamp

---
+++ internal/stamp/stamp.repository.go
@@ -0,0 +1,27 @@
+package stamp
+
+import (
+	"github.com/isd-sgcu/rpkm67-auth/internal/model"
+	"gorm.io/gorm"
+)
+
+type Repository interface {
+}
+
+type repositoryImpl struct {
+	Db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return &repositoryImpl{
+		Db: db,
+	}
+}
+
+func (r *repositoryImpl) FindByUserId(userId string, stamp *model.Stamp) error {
+	return r.Db.First(stamp, "userId = ?", userId).Error
+}
+
+func (r *repositoryImpl) StampByUserId(userId string, stamp *model.Stamp) error {
+	return r.Db.Model(stamp).Where("userId = ?", userId).Updates(stamp).Error
+}
---
+++ internal/stamp/stamp.service.go
@@ -0,0 +1,33 @@
+package stamp
+
+import (
+	"context"
+
+	proto "github.com/isd-sgcu/rpkm67-go-proto/rpkm67/backend/stamp/v1"
+	"go.uber.org/zap"
+)
+
+type Service interface {
+	proto.StampServiceServer
+}
+
+type serviceImpl struct {
+	proto.UnimplementedStampServiceServer
+	repo Repository
+	log  *zap.Logger
+}
+
+func NewService(repo Repository, log *zap.Logger) Service {
+	return &serviceImpl{
+		repo: repo,
+		log:  log,
+	}
+}
+
+func (s *serviceImpl) FindByUserId(_ context.Context, in *proto.FindByUserIdStampRequest) (res *proto.FindByUserIdStampResponse, err error) {
+	return nil, nil
+}
+
+func (s *serviceImpl) StampByUserId(_ context.Context, in *proto.StampRequest) (res *proto.StampResponse, err error) {
+	return nil, nil
+}
