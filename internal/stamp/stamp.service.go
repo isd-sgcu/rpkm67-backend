7bc8b5eedd4f4382e5bbdea90450b7b218640254
Thanpisit Naowapradit <thanpisit1403@gmail.com>
2024-06-30 22:13:11 +0700

    complete stamp-service & stamp map

--- internal/stamp/stamp.service.go
+++ internal/stamp/stamp.service.go
@@ -2,10 +2,13 @@ package stamp
 
 import (
 	"context"
+	"errors"
 
 	proto "github.com/isd-sgcu/rpkm67-go-proto/rpkm67/backend/stamp/v1"
 	"github.com/isd-sgcu/rpkm67-model/model"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Service interface {
@@ -14,14 +17,16 @@ type Service interface {
 
 type serviceImpl struct {
 	proto.UnimplementedStampServiceServer
-	repo Repository
-	log  *zap.Logger
+	repo            Repository
+	activityIdToIdx map[string]int
+	log             *zap.Logger
 }
 
-func NewService(repo Repository, log *zap.Logger) Service {
+func NewService(repo Repository, activityIdToIdx map[string]int, log *zap.Logger) Service {
 	return &serviceImpl{
-		repo: repo,
-		log:  log,
+		repo:            repo,
+		activityIdToIdx: activityIdToIdx,
+		log:             log,
 	}
 }
 
@@ -46,8 +51,15 @@ func (s *serviceImpl) StampByUserId(_ context.Context, in *proto.StampByUserIdRe
 		return nil, err
 	}
 
-	// stamp.Stamp string 00000000000, 01000100001
-	// in.ActivityId = "landmark-4"
+	actIdx, ok := s.activityIdToIdx[in.ActivityId]
+	if !ok {
+		return nil, status.Error(codes.Internal, errors.New("Invalid Activity ID").Error())
+	}
+
+	tempStrStamp := []byte(stamp.Stamp)
+	tempStrStamp[actIdx] = '1'
+	stamp.Stamp = string(tempStrStamp)
+	s.skillsCalc(stamp, actIdx)
 
 	err = s.repo.StampByUserId(in.UserId, stamp)
 	if err != nil {
@@ -66,5 +78,27 @@ func (s *serviceImpl) modelToProto(stamp *model.Stamp) *proto.Stamp {
 		PointC: int32(stamp.PointC),
 		PointD: int32(stamp.PointD),
 		Stamp:  stamp.Stamp,
+	}
+}
+
+func (s *serviceImpl) skillsCalc(stamp *model.Stamp, idx int) {
+	if idx <= 1 {
+		stamp.PointB += 2
+		stamp.PointD += 2
+	} else if idx == 2 || idx == 4 {
+		stamp.PointA++
+		stamp.PointB++
+		stamp.PointC += 2
+	} else if idx == 3 {
+		stamp.PointB += 2
+		stamp.PointC++
+		stamp.PointD++
+	} else if idx >= 4 && idx <= 6 {
+		stamp.PointA++
+		stamp.PointD++
+	} else if idx == 7 {
+		stamp.PointA++
+	} else {
+		stamp.PointA += 2
 	}
 }
